b3a6c024d33959da9c084e0eb81b6cfd250f0e19
zaienaji <zaien.aji@gmail.com>
2025-09-17 04:38:29 +0800

    init

---
+++ src/main/java/com/ws/dc/WiraDayCareApplication.java
@@ -0,0 +1,13 @@
+package com.ws.dc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiraDayCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiraDayCareApplication.class, args);
+	}
+
+}
